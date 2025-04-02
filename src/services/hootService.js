410989bbb9d3a8b9d39f2f1de585465271aca7cc
clairecleverlamb <clairechenszeying@gmail.com>
2025-04-02 03:39:05 -0700

    wip: adds service mod

--- src/services/hootService.js
+++ src/services/hootService.js
@@ -2,11 +2,14 @@ const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/hoots`;
 
 const index = async () => {
     try {
+        // config 
         const res = await fetch(BASE_URL, {
-            headers: { Authorization: `Bear ${localStorage.getItem('token')}`},
+            headers: { Authorization: `Bearer ${localStorage.getItem('token')}`},
         });
         return res.json();
     } catch (error) {
         console.log(error);
     }
 }
+
+export { index }; // name export syntax, (used to export multiple function )
