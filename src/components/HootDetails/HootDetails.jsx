9301e3abca2a5e015ecd3a01b162585f158debff
clairecleverlamb <clairechenszeying@gmail.com>
2025-04-02 04:45:15 -0700

    wip: added details component

--- src/components/HootDetails/HootDetails.jsx
+++ src/components/HootDetails/HootDetails.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect} from 'react';
-import {show} from '../../services//hootService'
+import { show } from '../../services/hootService';
 import { useParams } from "react-router";
 
 const HootDetails = (props) => {
@@ -8,16 +8,42 @@ const HootDetails = (props) => {
 
     useEffect(() => {
         const fetchHoot = async () => {
-            const hootData = await hootService.show(hootId);
+            const hootData = await show(hootId);
             setHoot(hootData);
         }
         fetchHoot();  // we need to run fetchHoot when run useEffect
     }, [hootId]);
-    console.log(hootId);
-    return 
+    if (!hoot) return <main>Loading....</main>
+
+    return (
         <main>
-        Hoot Details
-        </main>;
+            <section>
+                <header>
+                    <p>{hoot.category.toUpperCase()}</p>
+                    <h1>{hoot.title}</h1>
+                    <p>
+                        {`${hoot.author.username} posted on
+                        ${new Date(hoot.createdAt).toLocaleDateString()}`}
+                    </p>
+                </header>
+                <p>{hoot.text}</p>
+            </section>
+            <section>
+                <h2>Comments</h2>
+                {!hoot.comments.length && <p>There are no comments</p>}
+                {hoot.comments.map((comment) => (
+                    <article key={comment._id}>
+                        <header>
+                            <p>
+                              {`${comment.author.username} posted on
+                              ${new Date(comment.createdAt).toLocaleDateString()}`}
+                            </p>
+                        </header>
+                    </article>
+                ))}
+            </section>
+        </main>
+    ) 
   };
   
   export default HootDetails;
